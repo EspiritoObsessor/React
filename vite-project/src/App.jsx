a4850938d1e3f2d3e0d90c068f4ca93ef6519f31
EspiritoObsessor <leogaetaalves@gmail.com>
2024-08-19 17:19:48 -0300

    React

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import Header from "./components/header";
+import ListFruits from "./components/listFruits";
+
+import './App.css';
+import './scss/main.scss';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <ListFruits />
+    </>
+  )
+}
+
+export default App
